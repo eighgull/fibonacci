310eb64d7acb41a08aac08611c4de963465bfb71
eighgull <chmn7396@gmail.com>
2020-09-11 17:49:53 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 'use strict';
+const memo = new Map();
+// n=0 と n=1 は必ず使うので最初に配列に入れておく
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
+    // 配列にデータがあった場合の処理
+    // 配列にあるデータが使う
+    if (memo.has(n)) {
+        return memo.get(n);
     }
-    return fib(n - 1) + fib(n -2);
+    // 配列にデータがなかった場合の処理
+    // データがないのでまず計算する
+    const value = fib(n - 1) + fib(n - 2);
+    // 計算した結果を配列に入れる
+    memo.set(n, value);
+    return value; 
 }
 for (let i = 0; i <= 40; i++) {
     fib(i)
